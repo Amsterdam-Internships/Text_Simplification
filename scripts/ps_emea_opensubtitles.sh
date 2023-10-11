0e2d355a54607a77f4c33a4b1329f1b3a56a16bc
Daniel Vlantis <djvlantis@gmail.com>
2023-10-11 17:37:25 +0200

    Renamed mun_rand_model_3.yaml config file

--- scripts/ps_emea_opensubtitles.sh
+++ scripts/ps_emea_opensubtitles.sh
@@ -73,12 +73,12 @@ onmt_build_vocab -config config/model_en_c-s.yaml -overwrite True #Build vocab
 #OpenSubtitles
 touch $opensubtitles_folder/run/mun_rand_vocab.src; #create empty files where vocab will be stored
 touch $opensubtitles_folder/run/mun_rand_vocab.tgt;
-onmt_build_vocab -config config/mun_rand_model_3.yaml -overwrite True #build vocab
+onmt_build_vocab -config config/model_en-nl_opensubtitles_reduced.yaml -overwrite True #build vocab
 
 #Train Models
 #onmt_train -config config/model_nl-en_emea.yaml #-src_vocab $emea_folder/run/.vocab.src -tgt_vocab $emea_folder/run/.vocab.tgt #Train Emea model
 #onmt_train -config config/model_en_c-s.yaml #-src_vocab $wikisimple_folder/run/.vocab.src -tgt_vocab $wikisimple_folder/run/.vocab.tgt #Train WikiSimple model
-onmt_train -config config/mun_rand_model_3.yaml #-src_vocab NMT-Data/Model_English_S_Dutch_S/run/.vocab.src -tgt_vocab NMT-Data/Model_English_S_Dutch_S/run/.vocab.tgt #train OpenSubtitles Model
+onmt_train -config config/model_en-nl_opensubtitles_reduced.yaml #-src_vocab NMT-Data/Model_English_S_Dutch_S/run/.vocab.src -tgt_vocab NMT-Data/Model_English_S_Dutch_S/run/.vocab.tgt #train OpenSubtitles Model
 
 #Run input data through pipeline
 
