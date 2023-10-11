f1d2febc63c07f1eb6ca7e538837ae805ff5eb0c
Daniel Vlantis <djvlantis@gmail.com>
2023-10-11 15:26:34 +0200

    updated mun_model_1.yaml config file

--- scripts/mun_tfidf_pipe.sh
+++ scripts/mun_tfidf_pipe.sh
@@ -64,7 +64,7 @@ python3 notebooks/data_processing/train_dev_test_split.py 2000 2000 NMT-Data/Mod
 #EMEA
 touch $europarl_folder/run/mun_vocab.src; #create empty files where vocab will be stored
 touch $europarl_folder/run/mun_vocab.tgt;
-onmt_build_vocab -config config/mun_model_1.yaml -overwrite True #Build vocab
+onmt_build_vocab -config config/model_nl-en_europarl.yaml -overwrite True #Build vocab
 
 #WikiSimple
 touch $wikisimple_folder/run/vocab.src; #create empty files where vocab will be stored
@@ -77,7 +77,7 @@ touch $opensubtitles_folder/run/mun_vocab.tgt;
 onmt_build_vocab -config config/mun_model_3.yaml -overwrite True #build vocab
 
 #Train Models
-#onmt_train -config config/mun_model_1.yaml #-src_vocab $emea_folder/run/.vocab.src -tgt_vocab $emea_folder/run/.vocab.tgt #Train Emea model
+#onmt_train -config config/model_nl-en_europarl.yaml #-src_vocab $emea_folder/run/.vocab.src -tgt_vocab $emea_folder/run/.vocab.tgt #Train Emea model
 #onmt_train -config config/model_en_c-s.yaml #-src_vocab $wikisimple_folder/run/.vocab.src -tgt_vocab $wikisimple_folder/run/.vocab.tgt #Train WikiSimple model
 #onmt_train -config config/mun_model_3.yaml #-src_vocab NMT-Data/Model_English_S_Dutch_S/run/.vocab.src -tgt_vocab NMT-Data/Model_English_S_Dutch_S/run/.vocab.tgt #train OpenSubtitles Model
 
